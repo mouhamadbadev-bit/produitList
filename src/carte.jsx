0e7508c801337af0bac2f158399d46c6ec6efc3e
BA <mouhamadba.dev@gmail.com>
2026-09-16 15:09:53 +0100

    add projet fin

---
+++ src/carte.jsx
@@ -0,0 +1,89 @@
+import img1 from "./img01.png";
+import img2 from "./img02.png";
+import img3 from "./img03.png";
+import img4 from "./img04.png";
+import img5 from "./img05.png";
+import img6 from "./img06.png";
+import img7 from "./img07.png";
+import img8 from "./img08.png";
+import img9 from "./img09.png";
+
+
+
+const produits = [
+{
+id: 1,
+name: "Waffle with Berries" ,
+price:6.50 ,
+image: img1
+
+},
+
+{
+id: 2,
+name: "Vanilla Bean Crème" ,
+price: 7.00 ,
+image: img2
+
+},
+
+{
+id: 3 ,
+name:  "Macaron Mix of Five",
+price:8.00 ,
+image: img3
+
+},
+
+{
+id: 4,
+name: "Classic Tirramisu" ,
+price: 5.50 ,
+image: img4
+
+},
+
+{
+id: 5,
+name: "Pistachio Baklava",
+price: 4.00 ,
+image: img5
+
+},
+
+{
+id: 6,
+name: "Lemon Meringue" ,
+price: 5.00,
+image: img6
+
+},
+
+{
+id: 7,
+name: "Red velvet",
+price: 4.50,
+image: img7
+
+},
+
+{
+
+id: 8,
+name: "SaltedCaramel",
+price: 5.50,
+image: img8
+
+},
+
+{
+id: 9,
+name: "Vanilla Panna",
+price: 6.50,
+image: img9
+
+}
+
+]
+
+export default produits
